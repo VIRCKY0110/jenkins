40cafa54a409f1e21b5f59d3d7c3d80a00a358aa
VIRAJ <159519742+VIRCKY0110@users.noreply.github.com>
2024-10-20 11:46:31 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
-public static void main(String[] args) {
-System.out.println("****
-System.out.println("Hello from GITHub");
-System.out.println("**
-}
+    public static void main(String[] args) {
+        System.out.println("****");  // Closing quotation mark added
+        System.out.println("Hello from GITHub");  // No changes here, it was fine
+        System.out.println("**");  // Closing quotation mark added
+    }
 }
